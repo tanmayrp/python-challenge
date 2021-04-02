a19441c8bd57f852a5d9cf0a547c5d21712bfb48
tanmayrp <tanmayrp@gmail.com>
2021-04-02 16:19:05 -0400

    PyBank - Average Change (not working), Greatest Increase, Greatest Decrease calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,10 +5,16 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 totalMonths = 0
 totalProfitLoss = 0
 
+#Variables to calcualte change in P&L
+profitLossChangeDict = {"Date": [], "ProfitLossChange": []}
+previousProfitLoss = 0
+averageProfitLossChange = 0.0
 
-def calculateFinancialAnalysis(budgetData):
+def calculateFinancialAnalysis(budgetData, indexCounter):
     global totalMonths
     global totalProfitLoss
+    global previousProfitLoss
+    global profitLossChangeDict
 
     profitAndLoss = int(budgetData[1])
     
@@ -17,6 +23,30 @@ def calculateFinancialAnalysis(budgetData):
 
     # Net total amount of P&L over the entire period
     totalProfitLoss = totalProfitLoss + profitAndLoss
+
+    
+    if(indexCounter == 0):
+        previousProfitLoss = 0
+        profitLossChangeDict["Date"].append(str(budgetData[0]))
+        profitLossChangeDict["ProfitLossChange"].append(0)
+    else:
+        profitLossChangeDict["Date"].append(str(budgetData[0]))
+        profitLossChangeDict["ProfitLossChange"].append(int(budgetData[1]) - int(previousProfitLoss))
+
+    
+    previousProfitLoss = budgetData[1]
+
+def calculateProfitLossChangeAvg(profitLossChanges):
+    global profitLossChangeDict 
+
+    length = len(profitLossChangeDict["ProfitLossChange"])
+    averageProfitLossChange = 0.0
+    total = 0.0
+    
+    for profitLossChange in profitLossChanges:
+            total += profitLossChange
+
+    return total / length
 
 
 
@@ -27,17 +57,27 @@ headerTxt2 = "----------------------------"
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
-
+    indexCounter = 0
     # Read each row of data after the header and send it to function for analysis calculation
     for row in csvreader:
-        calculateFinancialAnalysis(row)
+        calculateFinancialAnalysis(row, indexCounter)
+        indexCounter += 1
 
-        
+averageProfitLossChange = calculateProfitLossChangeAvg(profitLossChangeDict["ProfitLossChange"])  
+greatestProfitLossIncrease = max(profitLossChangeDict["ProfitLossChange"])
+greatestProfitLossDecrease = min(profitLossChangeDict["ProfitLossChange"])
 
+# print(profitLossChangeDict)
 
 totalMonthsTxt = "Total Months:" + str(totalMonths)
 totalProfitLossTxt = "Total:" + str(totalProfitLoss)
+averageChangeTxt = "Average Change:" + str(averageProfitLossChange)
+greatestIncreaseTxt = "Greatest Increase in Profits: " + str(greatestProfitLossIncrease)
+greatestDecreaseTxt = "Greatest Decrease in Profits:: " + str(greatestProfitLossDecrease)
 print(headerTxt)
 print(headerTxt2)
 print(totalMonthsTxt)
 print(totalProfitLossTxt)        
+print(averageChangeTxt)
+print(greatestIncreaseTxt)
+print(greatestDecreaseTxt)
