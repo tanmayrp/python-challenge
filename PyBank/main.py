f1cbac1c333305ada958adb8da37cca2a4a223e8
tanmayrp <tanmayrp@gmail.com>
2021-04-02 17:36:15 -0400

    PyBank - Code Cleanup and provide commented code.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,35 +1,49 @@
+#Import the necessary files
 import os
 import csv
 
+#Initialize csv path to retrieve file
 csvpath = os.path.join('Resources', 'budget_data.csv')
+
+#Variables to calculate total months and net total PnL
 totalMonths = 0
 totalProfitLoss = 0
 
-#Variables to calcualte change in P&L
+#Variables to calcualte change in PnL
 dateList = []
 profitLossChangeList = []
-
 previousProfitLoss = 0
 averageProfitLossChange = 0.0
 
+# Function to calculate the Financial Analysis for Total Months, Net Total PnL, and keep running list of PnL Change
+# Parameters:
+#   budgetData - row of data rom the budget_data.csv; includes Date and PnL
+#   indexCounter - conuter to measure if this is the first row of data. 
 def calculateFinancialAnalysis(budgetData, indexCounter):
+
+    #defining global vars here. 
     global totalMonths
     global totalProfitLoss
     global previousProfitLoss
-
     global dateList
     global profitLossChangeList
 
-    profitAndLoss = int(budgetData[1])
+    # Add each row count to the totalMonths var to add up the number of months included in the dataset
+    totalMonths += 1
     
-     # Add each row count to the totalMonths var to add up the number of months included in the dataset
-    totalMonths = totalMonths+1
+    
+    #set PnL var
+    profitAndLoss = int(budgetData[1])
 
     # Net total amount of P&L over the entire period
-    totalProfitLoss = totalProfitLoss + profitAndLoss
+    totalProfitLoss += profitAndLoss
 
+
+    #dateList used to manage dates to be utilized for Min/Max print outs
     dateList.append(str(budgetData[0]))
 
+    #If its the first row of the CSV, set PnL change to - and append that 0 to the PnLChangeList
+    #PnLChangeList to be used for Avg change Pnl and Min/Max print outs
     if(indexCounter == 0):
         previousProfitLoss = 0
         profitLossChangeList.append(0)
@@ -38,6 +52,7 @@ def calculateFinancialAnalysis(budgetData, indexCounter):
     
     previousProfitLoss = budgetData[1]
 
+#Function to calculate the PnL Change Average
 def calculateProfitLossChangeAvg():
     global profitLossChangeList
 
@@ -50,33 +65,36 @@ def calculateProfitLossChangeAvg():
 
     return total / length
 
+##########################################################################################################
+######### MAIN FUNCTION
 
-#Headers to print to text file
-headerTxt = "Financial Analysis"
-headerTxt2 = "----------------------------"
+#CSV Management
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
+    csv_header = next(csvreader) #ignore header
     indexCounter = 0
-    # Read each row of data after the header and send it to function for analysis calculation
+
+    # Calculate Financial Analysis for each row
     for row in csvreader:
         calculateFinancialAnalysis(row, indexCounter)
         indexCounter += 1
 
+#Calculate Avg PNL change, and Min/Max values
 averageProfitLossChange = calculateProfitLossChangeAvg()  
 greatestProfitLossIncrease = max(profitLossChangeList)
 greatestProfitLossDecrease = min(profitLossChangeList)
 
-totalMonthsTxt = "Total Months:" + str(totalMonths)
+#Set string vars to be used to print to text and terminal
+headerTxt = "Financial Analysis"
+headerTxt2 = "----------------------------"
+totalMonthsTxt = "Total Months: " + str(totalMonths)
 totalProfitLossTxt = "Total: $" + str(totalProfitLoss)
 averageChangeTxt = "Average Change:" + str(round(averageProfitLossChange, 2))
-
-# valueIndex = greatestProfitLossIncrease["ProfitLossChange"].index()
 greatestIncreaseTxt = "Greatest Increase in Profits: " + str(dateList[profitLossChangeList.index(greatestProfitLossIncrease)]) + " ($" + str(greatestProfitLossIncrease) +  ")"
 greatestDecreaseTxt = "Greatest Decrease in Profits:: " + str(dateList[profitLossChangeList.index(greatestProfitLossDecrease)]) + " ($" + str(greatestProfitLossDecrease)+  ")"
 
+# Write data to text file
 textFilePath = os.path.join("analysis", "analysis.txt")
-
 fileObject = open(textFilePath, "w")
 fileObject.write(headerTxt + "\n")
 fileObject.write(headerTxt2 + "\n")
@@ -85,8 +103,9 @@ fileObject.write(totalProfitLossTxt + "\n")
 fileObject.write(averageChangeTxt + "\n")
 fileObject.write(greatestIncreaseTxt + "\n")
 fileObject.write(greatestDecreaseTxt + "\n")
-fileObject.close()
+fileObject.close() 
 
+#Write data to terminal
 print(headerTxt)
 print(headerTxt2)
 print(totalMonthsTxt)
@@ -94,3 +113,5 @@ print(totalProfitLossTxt)
 print(averageChangeTxt)
 print(greatestIncreaseTxt)
 print(greatestDecreaseTxt)
+
+##########################################################################################################
